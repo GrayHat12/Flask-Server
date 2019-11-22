92728df4546c0129710f32652e110400a7a5f2e9
GrayHat12 <grayhathacks10@gmail.com>
2019-11-22 16:02:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask , request
 import Article
 import GrayAPI
+import os
 import html
 from youtube import YouTube , Decoder
 
@@ -64,4 +65,5 @@ def youtube():
     
 
 if __name__ == '__main__': 
-	app.run() 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
